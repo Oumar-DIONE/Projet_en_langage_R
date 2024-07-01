8fb2b26b13ca76947c3083d5ac891ab345680a70
Oumar-DIONE <dioneomar014@gmail.com>
2024-07-01 11:58:49 +0000

    add script for preprocessing model2

---
+++ Projet1_120History_JO/data/preprocessing.R
@@ -0,0 +1,31 @@
+
+
+binarizer<-function(data,char_var)
+{
+  var_name=paste0(char_var,"_num")
+  data[[var_name]]=1
+  value1<-unique(data[[char_var]])[1]
+  value2<-unique(data[[char_var]])[2]
+  if (char_var %in% names(data))
+  {
+    data[data[[char_var]]==value1,var_name]=1
+    data[data[[char_var]]==value2,var_name]=0
+    data<-data[,!names(data) %in% c(char_var)]
+  }
+  
+  return(data)
+}
+# AVEC cette fonction, on va essayer de prédire le medail à partir des variables telles:
+# "Medal","Sex", "Age","Height","Weight","Season"
+
+preproc1<-function(data)
+{
+  df <- data[, c("Medal","Sex", "Age","Height","Weight","Season")]
+  df$category_Medal <- as.numeric(as.factor(df$Medal))
+  df<-binarizer(df,"Sex")
+  df<-binarizer(df,"Season")
+  df<-df[,!names(df) %in% c('Medal')]
+  return(df)
+  
+}
+model1_work_df<-preproc1(data_year_above_2000)
