14dd03b8809a889af571aef7118dc447ff5f6dbb
Oumar-DIONE <dioneomar014@gmail.com>
2024-07-02 15:21:31 +0000

    ,Add scrip senegal_case

---
+++ Projet2_AfricanFootbal/codes/Senegal_CASE.R
@@ -0,0 +1,2 @@
+senegal_data<-African_Nations_df[African_Nations_df$home_team=="Senegal" | African_Nations_df$away_team=="Senegal",]
+#senegal_data <- African_Nations_df[African_Nations_df$home_team == "Senegal" | African_Nations_df$away_team == "Senegal", ]
