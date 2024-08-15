7e9a872a7879114fd915688d4f8b977565f5bca7
Oumar-DIONE <dioneomar014@gmail.com>
2024-08-15 16:04:25 +0000

    customiz in module the former code

--- Solve_Optimization_Convex_Problems/Ordinary_Least_Squares.R
+++ Solve_Optimization_Convex_Problems/Ordinary_Least_Squares.R
@@ -27,3 +27,19 @@ prob_2<-Problem(Minimize(obj_2))
 result_2<-solve(prob_2)
 result_2$value
 result_2$getValue(x)
+
+# Exemple3 : OLS problem subject to beta component increasing with th index: that is for 1<=j<=p-1,
+# beta(j)<=beta(j+1)
+beta_3<-Variable(rows=p)
+obj_3<-sum((Y-X%*%beta_3)^2)
+prob_3<-Problem(Minimize(obj_3),list(diff(beta_3) >=0))
+result_3<-solve(prob_3)
+# optimal values of objective function
+result_3$value
+# optimal value for beta
+result_3$getValue(beta_3)
+# optimal dual values
+result_3$getDualValue(constraints(prob_3)[[1]])
+
+
+
