236414e99c4f3591a4bd78fa5cf6246bc3c9e0db
Oumar-DIONE <dioneomar014@gmail.com>
2024-08-26 18:46:15 +0000

    add a test for rep function available in the script test_on_rep

--- One_Smoothing_Spline_IV_Regression/Helper_functions.R
+++ One_Smoothing_Spline_IV_Regression/Helper_functions.R
@@ -1,4 +1,17 @@
 library(pracma)
+fun <- function(x,case=1)  
+{
+  switch(case,  
+         x,
+         x^2/sqrt(2),
+         sqrt(3*sqrt(3))*x*exp(-(x^2)/2),
+         sqrt(3)*x*sin(pi*x/2),
+         4*exp(-abs(x)) , 
+         log(abs(x-1)+1)*ifelse(x>1,1,-1)*sqrt(10/3),
+         sqrt(2)*x*cos(pi*x),
+         (log(abs(x-1)+1)*ifelse(x>1,1,-1)*sqrt(10/3) - 0.6*x+ (x^3)*2)/8
+  )
+}
 
 wmat <- function(x, h=1 ,ker='normal',knorm="sq",remove=FALSE)
 {
@@ -11,4 +24,17 @@ wmat <- function(x, h=1 ,ker='normal',knorm="sq",remove=FALSE)
   # principle diagonal of the matrix replaced with zeros
   if (remove==TRUE) wmat <-  wmat - diag(diag(wmat))
   wmat
+}
+
+tpsmat <- function(knots)
+{
+  k <- length(knots)
+  tmat <- rbind(pracma::ones(1,k),t(knots))
+  emat <- sapply(knots, function(x) abs(x-knots)^3)/12
+  
+  list(tmat=tmat,emat=emat)
+}
+bmat <- function(l,emat,tmat,W=pracma::eye(n)/n,n)
+{
+  rbind(cbind(W%*%emat + l*pracma::eye(n),W%*%t(tmat)),cbind(tmat,pracma::zeros(2)))
 }
