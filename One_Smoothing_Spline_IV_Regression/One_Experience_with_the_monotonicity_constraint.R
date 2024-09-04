400da9d2bf37dc51c6275e7bf1fc73490c785208
Oumar-DIONE <dioneomar014@gmail.com>
2024-09-04 11:31:31 +0000

    break time, save for secure

--- One_Smoothing_Spline_IV_Regression/One_Experience_with_the_monotonicity_constraint.R
+++ One_Smoothing_Spline_IV_Regression/One_Experience_with_the_monotonicity_constraint.R
@@ -17,7 +17,7 @@ kernel <- "laplace"
 set.seed(1234567)
 n <- as.numeric(N)
 sdu <- 1
-ll <- 20 #400
+ll <- 400 #400
 pmin <- .00001
 pmax <- .7
 pp <- seq(pmin,pmax,length=ll)
@@ -45,9 +45,7 @@ emat <- mats$emat
 
 D_0_S_mat<-retrieve_D_0_S_matrix(W,mats,x)
 bigmativ <- bmat(l,emat,tmat,W,n)
-bigW <- rbind(W,pracma::zeros(2,n))
-bigwy <- c(W%*%y,0,0)
-
+dim(x)
 # Installer CVXR si ce n'est pas déjà fait
 # install.packages("CVXR")
 library(CVXR)
@@ -56,7 +54,7 @@ library(CVXR)
 p <- 2    # Supposons p=5 pour l'exemple
 m<-n # Supposons n=1 pour l'exemple
 p_vector <- runif(m)  # Exemple de vecteur p de taille m
-Y <- runif(n)  # Exemple de vecteur Y de taille m
+Y <-  fun(x,case)  + u  # Exemple de vecteur Y de taille m
 C <- D_0_S_mat  # Matrice C de dimension (m, m + p)
 b <- 1  # b = 1 selon votre contrainte
 
@@ -70,28 +68,35 @@ objective <- Minimize(n - sum(sqrt(n * v)))
 constraints <- list(v >= 0, sum(v) == 1)
 
 # Produit d'Hadamard entre p et Y
-p_Y <- p_vector * Y  # vecteur de taille m
+p_Y <- v * Y  # vecteur de taille m
 
 # Compléter p_Y par des zéros pour obtenir un vecteur de taille m + p
 p_Y_extended <- c(p_Y, rep(0, p))  # vecteur de taille (m + p)
-
+print(length(p_Y_extended))
 # Convertir p_Y_extended en objet CVXR Constant pour pouvoir faire le produit matriciel
 p_Y_extended_cvxr <- Constant(p_Y_extended)
-
 # Ajouter la contrainte supplémentaire d'inégalité : C * (p * Y, 0) <= 0
 constraints <- append(constraints, list(C %*% p_Y_extended_cvxr >= 0))
 
+dim(C)
 # Formuler le problème d'optimisation
 problem <- Problem(objective, constraints)
 
-# Résoudre le problème
-result <- solve(problem)
+result <- tryCatch({
+  solve(problem)
+}, error = function(e) {
+  message("Error occurred: ", e$message)
+  NULL
+})
 
-# Afficher la solution
-cat("La solution optimale est:\n")
-print(result$getValue(v))
-cat("La valeur de l'objectif est:\n")
-print(result$value)
+if (!is.null(result)) {
+  cat("La solution optimale est:\n")
+  print(result$getValue(v))
+  cat("La valeur de l'objectif est:\n")
+  print(result$value)
+} else {
+  cat("Erreur lors de la résolution du problème.\n")
+}
 
 toc()
 
